3a8b7735c7be80af0d3fd895943d455ffea90843
Mamunbadsha <mamun.web77@gmail.com>
2025-05-12 17:41:31 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import ProductListPage from "./pages/ProductListPage";
+import BlogPage from "./pages/BlogPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import ProductDtailsPage from "./pages/ProductDtailsPage";
+import ContractPage from "./pages/ContractPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/product-list" element={<ProductListPage />} />
+      <Route path="/blog" element={<BlogPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/productdtails" element={<ProductDtailsPage />} />
+      <Route path="/contract" element={<ContractPage />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/ProductDtailsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductDtailsPage = () => {
+  return (
+    <div>ProductDtailsPage</div>
+  )
+}
+
+export default ProductDtailsPage
