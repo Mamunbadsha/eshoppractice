86964545cb09fd17a73ff0a46833a76ca4d6f68f
Mamunbadsha <mamun.web77@gmail.com>
2025-05-19 15:40:55 +0600

    dynamic done
    dynamic

--- src/components/FeatureProduct.jsx
+++ src/components/FeatureProduct.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import ProductLayout from './ProductLayout'
-import Container from './Container'
+import React from "react";
+import ProductLayout from "./ProductLayout";
+import Container from "./Container";
 
 const FeatureProduct = () => {
   return (
-   <Container>
-     <div className='flex'>
-        <ProductLayout/>
-        <ProductLayout/>
-        <ProductLayout/>
-        <ProductLayout/>
-        <ProductLayout/>
-    </div>
-   </Container>
-  )
-}
+    <Container>
+      <div className="flex gap-1">
+        <ProductLayout percentTag={true} category="LAPTOP" title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."rating= "5" totalrating="100" price="2,199.00" bg="yellow" />
+        <ProductLayout percentTag={true} category="LAPTOP" title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."rating= "5" totalrating="100" price="2,199.00" bg="pink" />
+        <ProductLayout percentTag={true} category="LAPTOP" title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."rating= "5" totalrating="100" price="2,199.00" bg="gray" />
+        <ProductLayout percentTag={true} category="LAPTOP" title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."rating= "5" totalrating="100" price="2,199.00" bg="orange" />
+        <ProductLayout percentTag={true} category="LAPTOP" title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."rating= "5" totalrating="100" price="2,199.00" bg="green" />
 
-export default FeatureProduct
+      </div>
+    </Container>
+  );
+};
+
+export default FeatureProduct;
--- src/components/ProductLayout.jsx
+++ src/components/ProductLayout.jsx
@@ -1,42 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "./Container";
 import { FaStar } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 
-const ProductLayout = () => {
+const ProductLayout = ({
+  percentTag,
+  category,
+  title,
+  rating,
+  totalrating,
+  price,
+  bg
+}) => {
+  let [ratingValue, setRatingValue] = React.useState(
+    new Array(+rating).fill(rating)
+  );
+
   return (
-    <div className=" border border-transparent hover:border-[#c3c3c3] duration-300 p-6 group rounded-lg ">
+    <div style={{background:bg}} className=" border border-transparent hover:border-[#c3c3c3] duration-300 p-6 group rounded-lg ">
       <div className="relative">
-        <img src="images/watch.png" alt="watch" />
-        <div className="bg-[#FF624C] py-[7px] px-[24px] absolute top-[-8px] right-[-9px] rounded-md font-bold text-white">50%</div>
-         <div className="flex absolute bottom-[6px] left-[46%] translate-x-[-50%] gap-5 scale-0 group-hover:scale-100 decoration-600">
-           <div className="w-[50px] h-[50px] rounded-full bg-white border border-[#FF624C] flex items-center justify-center text-[25px] text-[#FF624C] hover:bg-[#FF624C] hover:text-white duration-300 cursor-pointer">
-          <IoCartOutline />
+        <img className="w-full" src="images/watch.png" alt="watch" />
+        {percentTag && (
+          <div className="bg-[#FF624C] py-[7px] px-[24px] absolute top-[-8px] right-[-9px] rounded-md font-bold text-white">
+            50%
+          </div>
+        )}
+        <div className="flex absolute bottom-[6px] left-[46%] translate-x-[-50%] gap-5 scale-0 group-hover:scale-100 decoration-600">
+          <div className="w-[50px] h-[50px] rounded-full bg-white border border-[#FF624C] flex items-center justify-center text-[25px] text-[#FF624C] hover:bg-[#FF624C] hover:text-white duration-300 cursor-pointer">
+            <IoCartOutline />
+          </div>
+          <div className="w-[50px] h-[50px] rounded-full bg-white border border-[#FF624C] flex items-center justify-center text-[25px] text-[#FF624C] hover:bg-[#FF624C] hover:text-white duration-300 cursor-pointer">
+            <IoCartOutline />
+          </div>
+          <div className="w-[50px] h-[50px] rounded-full bg-white border border-[#FF624C] flex items-center justify-center text-[25px] text-[#FF624C] hover:bg-[#FF624C] hover:text-white duration-300 cursor-pointer">
+            <IoCartOutline />
+          </div>
         </div>
-           <div className="w-[50px] h-[50px] rounded-full bg-white border border-[#FF624C] flex items-center justify-center text-[25px] text-[#FF624C] hover:bg-[#FF624C] hover:text-white duration-300 cursor-pointer">
-          <IoCartOutline />
-        </div>
-           <div className="w-[50px] h-[50px] rounded-full bg-white border border-[#FF624C] flex items-center justify-center text-[25px] text-[#FF624C] hover:bg-[#FF624C] hover:text-white duration-300 cursor-pointer">
-          <IoCartOutline />
-        </div>
-         </div>
       </div>
       <div>
         <p className="font-['Montserrat'] font-normal text-sm leading-5 uppercase tracking-[5px] mt-[46px] mb-4">
-          L A P T O P
+          {category}
         </p>
         <h3 className="font-['Poppins'] font-semibold text-[20px] leading-[30px] group-hover:text-[#FF624c]   decoration-300">
-          S21 Laptop Ultra HD LED Screen Feature 2023 ...
+          {title}
         </h3>
         <div className="flex gap-2 items-center mt-1 mb-6">
-          <FaStar className="text-amber-400" />
-          <FaStar className="text-amber-400" />
-          <FaStar className="text-amber-400" />
-          <FaStar className="text-amber-400" />
-          <FaStar className="text-amber-400" />
-          <span>(100)</span>
+          {ratingValue.map((item, index) => (
+            <FaStar className="text-amber-400" key={index}/>
+          ))}
+          <span>({totalrating})</span>
         </div>
-        <p className=" text-[24px] font-extrabold">$1,199.00</p>
+        <p className=" text-[24px] font-extrabold">${price}</p>
       </div>
     </div>
   );
