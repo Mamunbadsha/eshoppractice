453047366212fc45df260a49da2f73bbfdd1cd20
Mamunbadsha <mamun.web77@gmail.com>
2025-05-20 00:57:55 +0600

    discount part done
    discount part done

--- src/components/FeatureProduct.jsx
+++ src/components/FeatureProduct.jsx
@@ -1,18 +1,101 @@
 import React from "react";
 import ProductLayout from "./ProductLayout";
 import Container from "./Container";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const FeatureProduct = () => {
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "gray" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "blue" }}
+        onClick={onClick}
+      />
+    );
+  }
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    // autopla
+  };
   return (
     <Container>
-      <div className="flex gap-1">
-        <ProductLayout percentTag={true} category="LAPTOP" title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."rating= "5" totalrating="100" price="2,199.00" bg="yellow" />
-        <ProductLayout percentTag={true} category="LAPTOP" title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."rating= "5" totalrating="100" price="2,199.00" bg="pink" />
-        <ProductLayout percentTag={true} category="LAPTOP" title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."rating= "5" totalrating="100" price="2,199.00" bg="gray" />
-        <ProductLayout percentTag={true} category="LAPTOP" title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."rating= "5" totalrating="100" price="2,199.00" bg="orange" />
-        <ProductLayout percentTag={true} category="LAPTOP" title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."rating= "5" totalrating="100" price="2,199.00" bg="green" />
-
-      </div>
+      <Slider {...settings}>
+        <ProductLayout
+          percentTag={true}
+          category="LAPTOP"
+          title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."
+          rating="5"
+          totalrating="100"
+          price="2,199.00"
+          bg="yellow"
+          stock={false}
+          stockAmount="50"
+        />
+        <ProductLayout
+          percentTag={false}
+          category="LAPTOP"
+          title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."
+          rating="3"
+          totalrating="100"
+          price="2,199.00"
+          bg="pink"
+          stock={true}
+          stockAmount="50"
+        />
+        <ProductLayout
+          percentTag={true}
+          category="LAPTOP"
+          title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."
+          rating="2"
+          totalrating="100"
+          price="2,199.00"
+          bg="gray"
+          stock={true}
+          stockAmount="50"
+        />
+        <ProductLayout
+          percentTag={true}
+          category="LAPTOP"
+          title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."
+          rating="4"
+          totalrating="100"
+          price="2,199.00"
+          bg="orange"
+          stock={true}
+          stockAmount="50"
+        />
+        <ProductLayout
+          percentTag={false}
+          category="LAPTOP"
+          title=" S21 Laptop Ultra HD LED Screen Feature 2023 ..."
+          rating="1"
+          totalrating="100"
+          price="2,199.00"
+          bg="green"
+          stock={false}
+          stockAmount="50"
+        />
+      </Slider>
     </Container>
   );
 };
--- src/components/ProductLayout.jsx
+++ src/components/ProductLayout.jsx
@@ -10,22 +10,33 @@ const ProductLayout = ({
   rating,
   totalrating,
   price,
-  bg
+  bg,
+  stock,
+  stockAmount,
 }) => {
   let [ratingValue, setRatingValue] = React.useState(
     new Array(+rating).fill(rating)
   );
 
   return (
-    <div style={{background:bg}} className=" border border-transparent hover:border-[#c3c3c3] duration-300 p-6 group rounded-lg ">
+    <div
+      style={{ background: bg }}
+      className=" border border-transparent hover:border-[#c3c3c3] duration-300 p-6 group rounded-lg "
+    >
       <div className="relative">
         <img className="w-full" src="images/watch.png" alt="watch" />
-        {percentTag && (
+        {percentTag ? (
           <div className="bg-[#FF624C] py-[7px] px-[24px] absolute top-[-8px] right-[-9px] rounded-md font-bold text-white">
             50%
           </div>
+        ) : (
+          <div className="bg-[#482b8d]  h-[70px] w-[70px] rounded-full py-[7px] px-[24px] absolute top-[-8px] right-[-9px]  font-bold text-white">
+            <span className=" absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+              50%
+            </span>
+          </div>
         )}
-        <div className="flex absolute bottom-[6px] left-[46%] translate-x-[-50%] gap-5 scale-0 group-hover:scale-100 decoration-600">
+        <div className="flex absolute bottom-[6px] left-[46%] translate-x-[-50%] gap-5 scale-0 group-hover:scale-100 duration-500">
           <div className="w-[50px] h-[50px] rounded-full bg-white border border-[#FF624C] flex items-center justify-center text-[25px] text-[#FF624C] hover:bg-[#FF624C] hover:text-white duration-300 cursor-pointer">
             <IoCartOutline />
           </div>
@@ -41,16 +52,24 @@ const ProductLayout = ({
         <p className="font-['Montserrat'] font-normal text-sm leading-5 uppercase tracking-[5px] mt-[46px] mb-4">
           {category}
         </p>
-        <h3 className="font-['Poppins'] font-semibold text-[20px] leading-[30px] group-hover:text-[#FF624c]   decoration-300">
+        <h3 className="font-['Poppins'] font-semibold text-[20px] leading-[30px] group-hover:text-[#FF624c] group-hover:underline  duration-500">
           {title}
         </h3>
         <div className="flex gap-2 items-center mt-1 mb-6">
           {ratingValue.map((item, index) => (
-            <FaStar className="text-amber-400" key={index}/>
+            <FaStar className="text-amber-400" key={index} />
           ))}
           <span>({totalrating})</span>
         </div>
         <p className=" text-[24px] font-extrabold">${price}</p>
+        {stock && (
+          <div className="w-full h-[30px] bg-[#ddd] rounded-[25px] relative">
+            <div className="w-1/2 h-[30px]  bg-[#233066] rounded-[25px] text-center "></div>
+            <p className="absolute top-[50%] left-[50%] translate-x-[-78%] translate-y-[-57%]">
+              {stockAmount} AVAILABLE
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
