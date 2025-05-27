e34cb3e8d63e6e4499f36550013e32199ed7bf17
Mamunbadsha <mamun.web77@gmail.com>
2025-05-27 19:52:47 +0600

    homework
    done

--- src/components/BrandName.jsx
+++ src/components/BrandName.jsx
@@ -5,44 +5,39 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 const BrandName = () => {
-  //   var settings = {
-  //     dots: false,
-  //     infinite: true,
-  //     speed: 500,
-  //     slidesToShow: 4,
-  //     slidesToScroll: 1,
-  //     nextArrow: <SampleNextArrow />,
-  //     prevArrow: <SamplePrevArrow />,
-  //     autoplay: true,
-  //     autoplaySpeed: 1000,
-  //   };
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
 
   return (
-    <div>
-      <Container>
-        <div className="flex justify-between py-[100px]">
-          <div>
-            <img src="images/brand1.png" alt="" />
-          </div>
-          <div>
-            {" "}
-            <img src="images/brand2.png" alt="" />
-          </div>
-          <div>
-            {" "}
-            <img src="images/brand3.png" alt="" />
-          </div>
-          <div>
-            {" "}
-            <img src="images/brand4.png" alt="" />
-          </div>
-          <div>
-            {" "}
-            <img src="images/brand1.png" alt="" />
-          </div>
+    <Container>
+      <Slider {...settings} className="mb-[80px] justify-center">
+        <div>
+          <img src="images/brand1.png" alt="" />
         </div>
-      </Container>
-    </div>
+        <div>
+          {" "}
+          <img src="images/brand2.png" alt="" />
+        </div>
+        <div>
+          {" "}
+          <img src="images/brand3.png" alt="" />
+        </div>
+        <div>
+          {" "}
+          <img src="images/brand4.png" alt="" />
+        </div>
+        <div>
+          {" "}
+          <img src="images/brand1.png" alt="" />
+        </div>
+      </Slider>
+    </Container>
   );
 };
 
